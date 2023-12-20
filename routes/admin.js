0cc89c49b70f83a60cb2b54d149047a9ae6208ce
wayungi <fwayungi.com>
2023-12-20 22:59:15 +0300

    ADD: routes files

---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const route =  express.Router()
+const path =  require('path')
+
+// add restaurnat
+
+//add food
+
+// delete restaurant
+
+//delete food
+
+// disable restaurant
+
+//edit food / restaurant
+
+
+module.exports = router
---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const express =  require('express')
+const route =  express.Router()
+const path =  require('path')
+
+// view favuorite restaurnats
+
+//view all restaurants - paginated
+
+//mark restuarant as favaourite
+
+//view menu from a restaurant
+
+//view particular food on menu of all reataunarnts
+
+// make order(add to cart), include your location using google maps
+
+// checkout on the cart
+
+
+module.exports = router;
