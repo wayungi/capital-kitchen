af8b91d487f56863b50dec948fe40df789e0f0d7
wayungi <fwayungi.com>
2023-12-21 12:39:33 +0300

    ADD: get menu items for a single restaurant

---
+++ public/data/menu.js
@@ -0,0 +1,38 @@
+const data = [
+    { 
+        "id": "1",
+        "name": "beef steak",
+        "price": 12000,
+        "restaurant": "1",
+        "path": "https://picsum.photos/200",
+        "desc": "Grilled steaks"
+    },
+    {
+        "id": "2",
+        "name": "chicken",
+        "price": 5000,
+        "restaurant": "1",
+        "path": "https://picsum.photos/200",
+        "desc": "Freshly deep fried chicken thigh"
+    },
+
+    {
+        "id": "3",
+        "name": "chicken",
+        "price": 6000,
+        "restaurant": "2",
+        "path": "https://picsum.photos/200",
+        "desc": "Freshly roasted chicken wing / thigh"
+    },
+    {
+        "id": "4",
+        "name": "pork",
+        "price": 15000,
+        "restaurant": "2",
+        "path": "https://picsum.photos/200",
+        "desc": "Roasted pork stick"
+    }
+
+]
+
+module.exports = data
--- routes/menu.js
+++ routes/menu.js
@@ -1,31 +1,56 @@
 const express =  require('express')
 const router = express.Router()
 const path = require('path')
+const {v4: uuid } = require('uuid')
 
-// router.route('/:restaurantId')
-//     //add food
-//     .post((req, res) => {
+const data = {}
+data.menu = require('../public/data/menu')
 
-//     })
+router.route('/:restaurantId')
+    //add food
+    .post((req, res) => {
+        const { name, price, restaurant, path, desc } =  req.body
+        if(name === '' || price === '' || restaurant === '' || path === '' || desc === ''){
+            res.json({
+                "response": "Fill in all fields"
+            })
+        }
+        const menuItem = {
+            "id": uuid(),
+            name,
+            price,
+            "restaurant": req.params.restaurantId,
+            path,
+            desc
+        }
+        data.menu = [...data.menu, menuItem]
+        res.json({"response": menuItem})
+    })
 
-//     //get menu from a restaurant
-//     .get((req, res) => {
+    // get restaurant menus
+    .get((req, res) => {
+        console.log(data.menu)
+        const restaurantMenu = data.menu.filter((menuItem) => menuItem.restaurant === req.params.restaurantId) 
+        console.log(restaurantMenu)
+        if(!restaurantMenu) res.status(200).json({"response": "No menu items found"})
+        res.status(200).json({"response": restaurantMenu})
+    })
 
-//     })
+    // //edit food
+    // .patch((req, res) => {
 
-//     //edit food
-//     .patch((req, res) => {
+    // })
 
-//     })
+    // //delete food
+    // .delete((req, res) => {
 
-//     //delete food
-//     .delete((req, res) => {
+    // })
 
-//     })
+    // // order food
+    // .post('/order', (req, res) => {
 
-//     // order food
-//     .post('/order', (req, res) => {
+    // })
 
-//     })
+    // get menu from a single restaurant
 
 module.exports = router
