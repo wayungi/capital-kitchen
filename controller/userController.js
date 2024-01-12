8875c2db50ff985c2598f4545bf501fede25b3b9
wayungi <fwayungi.com>
2024-01-12 23:11:19 +0300

    ADD: handleLogout method

--- controller/userController.js
+++ controller/userController.js
@@ -95,8 +95,34 @@ const genAccessToken = (req, res) => {
     );
 }
 
+const handleLogout = (req, res) => {
+    // delete access token on client
+
+    //check for refreshtoken in req
+    const cookies =  req.cookies
+    if(!cookies?.jwt) return  res.sendStatus(204) //No content
+    const refreshToken =  cookies.jwt
+
+    //find the user with the refreshToken ||  check for refreshtoken in db
+    const foundUser =  UsersDB.users.find((user) => user.refreshToken === refreshToken)
+    if(!foundUser){
+        res.clearCookie('jwt', { httpOnly: true, maxAge: 24*60*60*1000, /*secure: true*/ })
+        return res.sendStatus(204)
+    }
+
+    // delete refreshtoken from db 
+    UsersDB.setUsers([
+        ...UsersDB.users.filter((user) => user.refreshToken !== refreshToken), 
+        {...foundUser, refreshToken: ''}
+    ])
+    res.clearCookie('jwt', { httpOnly: true, maxAge: 24*60*60*1000, /*secure: true*/ }) // secure: true servers on https/ in production
+    res.sendStatus(204)
+
+}
+
 module.exports = { 
     handleRegistration,
     handleLogin,
-    genAccessToken
+    genAccessToken,
+    handleLogout
 }
