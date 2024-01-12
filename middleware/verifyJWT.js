669356f444d5fe0722ac819dbee4efe072c899ae
wayungi <fwayungi.com>
2024-01-12 12:14:17 +0300

    ADD: middleware to verify users

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const verifyJWT = (req, res, next) => {
     const authHeader =  req.headers['authorization']
     if(!authHeader) res.sendStatus(401)
-    console.log(authHeader)
+    // console.log(authHeader)
     const token = authHeader.split(' ')[1]
     jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, decoded) => {
         if(err) res.sendStatus(403)
