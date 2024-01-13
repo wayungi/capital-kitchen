9c31ba84a6ad0fd0ca05496e8ff2667b1fc4cd58
wayungi <fwayungi.com>
2024-01-13 11:12:12 +0300

    UPDATE: registration implementation

--- controller/registerControlletr.js
+++ controller/registerControlletr.js
@@ -1,5 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
 const bcrypt =  require('bcrypt')
+const path = require('path')
+const fsPromises =  require('fs').promises
 const UsersDB =  {
     users: require('../model/users.json'),
     setUsers: function (data) {
@@ -7,21 +9,21 @@ const UsersDB =  {
     }
 }
 
-const handleRegistration = (req, res) => {
+const handleRegistration =  async (req, res) => {
     const { username, password } =  req.body
     if(!username || !password) return res.status(400).json({'message': 'Username & Password are required'})
     const duplicate = UsersDB.users.find((person) => person.username === username)
     if(duplicate) return res.sendStatus(409) // conflict
     const saltRounds = 10
-    bcrypt.genSalt(saltRounds, (err, salt) => {
-        bcrypt.hash(password, salt, (err, hash) => {
-            if(err) return res.sendStatus(500)
-            const newUser = {id: uuidv4(), username, password: hash}
-            UsersDB.setUsers([...UsersDB.users, newUser])
-            console.log(newUser)
-            res.status(201).json({'message': `${newUser.username} created`})
-        });
-    });
+    const hash = await bcrypt.hash(password, saltRounds)
+    if(!hash) return res.sendStatus(500) 
+    const newUser = {id: uuidv4(), username, password: hash}
+    UsersDB.setUsers([...UsersDB.users, newUser])
+    console.log(UsersDB.users)
+    res.status(201).json({'message': `${newUser.username} created`})
+
+
+    
 }
 
 module.exports = handleRegistration
