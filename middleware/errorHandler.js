0ce344efc8f9c7420d59cb22fde07b3fac502543
wayungi <fwayungi.com>
2024-01-15 15:06:19 +0300

    ADD: return statement to on internal server error

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -4,8 +4,7 @@ const { logEvent } = require('./logEvent')
 
 const errorHandler = (err, req, res, next) => {
     logEvent(`${err.name}\t${err.message}`, 'errorLog.txt')
-    // console.error(err.stack)
-    res.status(500).send(err.message)
+    return res.status(500).send(err.message)
     next()
 }
 
