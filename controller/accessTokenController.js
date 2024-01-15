646e2ee4ddf3f1673282f561ef524580e0aa7a35
wayungi <fwayungi.com>
2024-01-15 12:08:08 +0300

    ADD: roles at refreshToken recreation

--- controller/accessTokenController.js
+++ controller/accessTokenController.js
@@ -1,33 +1,38 @@
-const UsersDB =  {
-    users: require('../model/users.json'),
-    setUsers: function (data) {
-        this.users =  data
-    }
-}
+const UsersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
 
 //jwt
-const jwt =  require('jsonwebtoken')
-require('dotenv').config()
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const handleAccessToken = (req, res) => {
-    const cookies =  req.cookies
-    if(!cookies?.jwt) return res.sendStatus(401)
-    const refreshToken =  cookies.jwt
-    const foundUser =  UsersDB.users.find((user) => user.refreshToken === refreshToken)
-    if(!foundUser) return res.sendStatus(403)
-    jwt.verify(
-        refreshToken,
-        process.env.REFRESH_TOKEN_SECRET,
-        (err, decoded) => {
-            if(err || foundUser.username !== decoded.username) return res.sendStatus(403)
-            const accessToken =  jwt.sign(
-                {"username": decoded.username},
-                process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '30s' }
-            )
-            res.json({ accessToken })
-        }
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(401);
+  const refreshToken = cookies.jwt;
+  const foundUser = UsersDB.users.find(
+    (user) => user.refreshToken === refreshToken
+  );
+  if (!foundUser) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+    if (err || foundUser.username !== decoded.username)
+      return res.sendStatus(403);
+    const roles = Object.values(foundUser.roles);
+    const accessToken = jwt.sign(
+      {
+        UserInfo: {
+          username: decoded.username,
+          roles: roles,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "30s" }
     );
-}
+    res.json({ accessToken });
+  });
+};
 
-module.exports = handleAccessToken
+module.exports = handleAccessToken;
