26359c84d3657c0291d6a68263be260c5d3223c4
wayungi <fwayungi.com>
2024-01-15 14:37:58 +0300

    FORMAT: format restaurnat.js with prettier

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,25 +1,24 @@
-const express = require('express')
-const router =  express.Router()
+const express = require("express");
+const router = express.Router();
 const {
-    addRestaurant,
-    getAllRestaurants,
-    updateRestaurantData,
-    deleteRestaurant,
-    disableRestaurant,
-    enableRestaurant
-} = require('../controller/restaurantController')
-const { verifyJWT } =  require('../middleware/verifyJWT')
+  addRestaurant,
+  getAllRestaurants,
+  updateRestaurantData,
+  deleteRestaurant,
+  disableRestaurant,
+  enableRestaurant,
+} = require("../controller/restaurantController");
+const { verifyJWT } = require("../middleware/verifyJWT");
 
+router
+  .route("/")
+  .post(verifyJWT, addRestaurant) // add restaurnat
+  .get(getAllRestaurants); // get all restaurants
+router
+  .route("/:id")
+  .put(verifyJWT, updateRestaurantData) //edit restaurant
+  .delete(verifyJWT, deleteRestaurant); // delete restaurant
+router.route("/:id/disable").post(verifyJWT, disableRestaurant); // disable restaurant
+router.route("/:id/enable").post(verifyJWT, enableRestaurant); // enable restaurant
 
-router.route('/')
-    .post(verifyJWT, addRestaurant)    // add restaurnat
-    .get( getAllRestaurants)    // get all restaurants
-router.route('/:id')
-    .put(verifyJWT, updateRestaurantData)    //edit restaurant
-    .delete(verifyJWT, deleteRestaurant)    // delete restaurant
-router.route('/:id/disable')
-    .post(verifyJWT, disableRestaurant)    // disable restaurant
-router.route('/:id/enable')
-    .post(verifyJWT, enableRestaurant)    // enable restaurant
-
-module.exports = router
+module.exports = router;
