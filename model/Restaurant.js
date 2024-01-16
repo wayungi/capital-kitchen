39b1a964e7587a68407b031c89098ea0645669cc
wayungi <fwayungi.com>
2024-01-16 14:23:58 +0300

    RENAME: capitalize restaurant schema file

--- model/Restaurant.js
+++ model/Restaurant.js
@@ -14,6 +14,8 @@ module.exports = mongoose.model('Restaurant', restaurantSchema)
     Just like in react, the file name is capitalized
 
     first argument to monggoose.model is Capitalized and is not pluralised - convetion
+
+    the collection created will then be 'employees'. NOTE: the lowercase and plural
     
     
     
