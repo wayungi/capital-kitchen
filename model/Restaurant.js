c9523f3b693aba11c96a920848ce71069391b119
wayungi <fwayungi.com>
2024-01-16 14:19:22 +0300

    ADD: restaurant schema

---
+++ model/Restaurant.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const restaurantSchema = new Schema({
+    name: string,
+    location: string,
+    status: string
+});
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
+
+
+/* 
+    Just like in react, the file name is capitalized
+
+    first argument to monggoose.model is Capitalized and is not pluralised - convetion
+    
+    
+    
+*/
